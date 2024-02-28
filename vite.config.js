aa023b43be347295b690b97c67bc3358a7545765
dawadi_pratima <dawadipratima72@gmail.com>
2024-02-29 00:22:55 +0545

    configuration changed

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/',
   build: {
     outDir: 'public', // Specify the output directory
     emptyOutDir: true, // Clear the output directory before building
+    
   },
 })

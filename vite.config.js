28f3de1b822b31f3fd94ff92668fad35270e2fdd
dawadi_pratima <dawadipratima72@gmail.com>
2024-02-29 00:33:27 +0545

    cv uploaded

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
-  build: {
-    outDir: 'public', // Specify the output directory
-    emptyOutDir: true, // Clear the output directory before building
-    
-  },
+
 })

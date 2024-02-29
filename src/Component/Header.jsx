e64fd47486a65d3f7c5e5ace679077733797380f
dawadi_pratima <dawadipratima72@gmail.com>
2024-02-29 07:22:52 +0545

    updated code

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -86,7 +86,7 @@ const Header = () => {
               <button className="button-resume fw-bold btn rounded-0 mt-4">
                 <a
                   className="text-decoration-none"
-                  href="./public/Pratima_Dawadi(CV).pdf"
+                  href="/cv.pdf"
                   download={true}
                   target="_blank"
                   rel="noreferrer"
